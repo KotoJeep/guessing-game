68b632871a6ce709e4ccf1fd8d2d81ea7cb50eed
KotoJeep <omaigader@gmail.co>
2021-09-01 09:44:58 +0700

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  
+  constructor() {
+    let middle
+  }
+  
+  setRange(min, max) {
+    this.min = min
+    this.max = max
+  }
+  
+  guess() {
+    this.middle = Math.round((this.min + this.max) / 2)
+    return this.middle
+  }
+  
+  lower() {
+    this.max = this.middle
+    return this.guess()
+  }
+  
+  greater() {
+    this.min = this.middle
+    return this.guess()
+  }
 }
 
 module.exports = GuessingGame;
